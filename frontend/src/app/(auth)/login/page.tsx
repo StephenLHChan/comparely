3d8aad13bd2a81c57ae781fb055ad311bc7b7653
StephenLHChan <stephenc.1018@gmail.com>
2024-12-24 18:20:05 -0500

    feat(auth): register form init

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 import { LoginForm } from "@/components/auth/login-form";
 
-const LoginPage: React.FC = () => {
+const LoginPage = () => {
   return (
     <div className="flex w-full max-w-sm flex-col gap-6">
       <LoginForm />
---
+++ frontend/src/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "@/components/auth/register-form";
+
+const RegisterPage = () => {
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-6">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
