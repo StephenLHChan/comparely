d24b097d31290c62698391e22de44c21f06e0277
StephenLHChan <stephenc.1018@gmail.com>
2024-12-27 17:13:09 -0500

    feat(frontend): :sparkles: navbar init

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+import { Logo } from "@/components/logo";
+
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+import { icons, LogIn, Menu } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const pathname = usePathname();
+
+  return (
+    <header className="sticky top-2 lg:top-5 z-40">
+      <div className="container mx-auto">
+        <div className="bg-opacity-15 border rounded-2xl flex justify-between items-center p-2 bg-background/70 backdrop-blur-sm">
+          <Logo />
+          {/* <!-- Mobile --> */}
+          <div className="flex items-center lg:hidden">
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger asChild>
+                <Menu
+                  onClick={() => setIsOpen(!isOpen)}
+                  className="cursor-pointer lg:hidden"
+                />
+              </SheetTrigger>
+
+              <SheetContent
+                side="left"
+                className="flex flex-col justify-between rounded-tr-2xl rounded-br-2xl bg-card border-secondary"
+              >
+                <div>
+                  <SheetHeader className="mb-4 ml-4">
+                    <SheetTitle className="flex items-center"></SheetTitle>
+                  </SheetHeader>
+
+                  <div className="flex flex-col gap-2"></div>
+                </div>
+
+                <SheetFooter className="flex-col sm:flex-col justify-start items-start">
+                  <Separator className="mb-2" />
+                </SheetFooter>
+              </SheetContent>
+            </Sheet>
+          </div>
+
+          {/* <!-- Desktop --> */}
+          <NavigationMenu className="hidden lg:block mx-auto">
+            <NavigationMenuList className="space-x-0">
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="bg-transparent hover:!bg-transparent">
+                  Products
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <div className="w-[300px] gap-5 p-2">
+                    <ul className="flex flex-col"></ul>
+                  </div>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem></NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+
+          {pathname !== "/login" && (
+            <div className="hidden lg:flex mr-2">
+              <Button asChild size="sm" className="ms-2" aria-label="Login">
+                <Link aria-label="Login" href="/login">
+                  Login
+                  <LogIn />
+                </Link>
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
