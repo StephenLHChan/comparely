f8994bdc0ae5ad69b9fe810a201504ff42d081d8
StephenLHChan <stephenc.1018@gmail.com>
2024-12-28 16:34:25 -0500

    feat: generateAvatarFallback init

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,36 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateAvatarFallback(string: string) {
+  const names = string.split(" ").filter((name: string) => name);
+  const mapped = names.map((name: string) => name.charAt(0).toUpperCase());
+
+  return mapped.join("");
+}
+
+export function generateMeta({
+  title,
+  description,
+  canonical,
+}: {
+  title: string;
+  description: string;
+  canonical: string;
+}): Metadata {
+  return {
+    title: `${title} - Shadcn UI Kit`,
+    description: description,
+    metadataBase: new URL(`${process.env.BASE_URL}`),
+    alternates: {
+      canonical: `/dashboard${canonical}`,
+    },
+    openGraph: {
+      images: [`${process.env.DASHBOARD_BASE_URL}/seo.jpg`],
+    },
+  };
 }
