1c0fb18f341f9c4d92abddf4ae6a83227a13ba74
StephenLHChan <stephenc.1018@gmail.com>
2024-12-28 23:23:46 -0500

    fix: eslint error

--- frontend/src/app/(protected)/admin/user/page.tsx
+++ frontend/src/app/(protected)/admin/user/page.tsx
@@ -5,7 +5,6 @@ import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/data-table";
 
 import { CirclePlus } from "lucide-react";
-import { User } from "@/data/user";
 
 const UserPage = async () => {
   const users = await getUsers();
