44d63813d355c3229623bef00108b8a80f21f37d
StephenLHChan <stephenc.1018@gmail.com>
2024-12-29 13:07:58 -0500

    feat(auth): add logo to auth forms

--- frontend/src/components/logo.tsx
+++ frontend/src/components/logo.tsx
@@ -3,13 +3,26 @@ import React from "react";
 
 import { Copyleft } from "lucide-react";
 
-export const Logo = () => {
+interface LogoProps {
+  disableLink?: boolean;
+}
+
+export const Logo = ({ disableLink }: LogoProps) => {
+  const LogoCore = () => {
+    return (
+      <div className="flex font-bold items-center">
+        <span className="flex items-center justify-center size-7 lg:size-8 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-secondary">
+          <Copyleft className="size-5 lg:size-6 text-white" />
+        </span>
+        <h5 className="text-lg lg:text-xl">comparely</h5>
+      </div>
+    );
+  };
+  if (disableLink) return <LogoCore />;
+
   return (
-    <Link href="/" className="flex font-bold items-center">
-      <span className="flex items-center justify-center size-7 lg:size-8 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-secondary">
-        <Copyleft className="size-5 lg:size-6 text-white" />
-      </span>
-      <h5 className="text-lg lg:text-xl">comparely</h5>
+    <Link href="/">
+      <LogoCore />
     </Link>
   );
 };
